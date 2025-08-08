8b3673450c39cf71110fc60e1dce903b27241a08
Alexioscreed <lhusajo.ngulwa@gmail.com>
2025-08-08 15:05:03 +0300

    feat: add separate vite config for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
   plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
           chakra: ['@chakra-ui/react', '@emotion/react', '@emotion/styled'],
-          router: ['react-router-dom'],
-        },
-      },
-    },
+          router: ['react-router-dom']
+        }
+      }
+    }
   },
   server: {
     port: 5173,
-    host: true,
-  },
-});
+    host: true
+  }
+})

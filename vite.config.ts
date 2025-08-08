67e5f997f32d56705ae80e40e45887a47ea9fecf
Alexioscreed <lhusajo.ngulwa@gmail.com>
2025-08-08 15:14:00 +0300

    refactor: remove all custom build configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          chakra: ['@chakra-ui/react', '@emotion/react', '@emotion/styled'],
-          router: ['react-router-dom']
-        }
-      }
-    }
-  },
-  server: {
-    port: 5173,
-    host: true
-  }
 })

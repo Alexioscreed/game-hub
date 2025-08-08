b296f27937e3e7034c9a5ac1b8e6e88ff94fb2a6
Alexioscreed <lhusajo.ngulwa@gmail.com>
2025-08-08 15:00:06 +0300

    fix: explicitly set root and input in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '.',
   plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
           chakra: ['@chakra-ui/react', '@emotion/react', '@emotion/styled'],
-          router: ['react-router-dom']
-        }
-      }
-    }
+          router: ['react-router-dom'],
+        },
+      },
+    },
   },
   server: {
     port: 5173,
-    host: true
-  }
-})
+    host: true,
+  },
+});
